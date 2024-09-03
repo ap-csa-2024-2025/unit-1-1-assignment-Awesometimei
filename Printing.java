9021b31c27cfce27697af1b2ffd7135f2442245c
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-09-03 10:03:53 -0700

    skibidi

---
+++ Printing.java
@@ -0,0 +1,16 @@
+public class Printing 
+{
+    public static void main(String[] args)
+    {
+        //int, double, boolean, String
+        int myAge = 3;          //int; whole numbers
+        double price = 4.99;     //double; decimal numbers (float)
+
+        boolean isGood; 
+        isGood = false;
+
+        String name = "Danny Nguyen";
+
+        System.out.println("Hello my name is " + name + "!");
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
-
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while (i < 3)
+        {
+            System.out.println("Coding is fun!");
+            i++;
+        }
+    }
+}
